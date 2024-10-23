b48961572e16249b855c88698b5d47bab9b0180c
imegazek <finisherman14@gmail.com>
2024-10-23 19:15:37 +0300

    Update main.py

--- baglanti/main.py
+++ baglanti/main.py
@@ -3,7 +3,10 @@ import streamlit as st
 st.ttlie("Bağlantı Güvenliği Kontrol Aracı")
 
 st.html(
-    
+    <head>
+    <meta name="google-site-verification" content="xMaXkppfXd2YK8ERY_6Jom-rc4Rsb-F1AyuM5_vNe-E" />
+    <title>Bağlantım Güvenli mi?<title>
+    <head>
 )
 
 url = st.text_input(" ")
