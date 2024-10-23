7dd598ff3c58107cbc77e4b8f9a39501c53ac9f9
imegazek <finisherman14@gmail.com>
2024-10-23 18:07:37 +0300

    Add files via upload

---
+++ baglantı kontrol/main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+url = st.text_input(" ")
+
+if url.startswith("https"):
+    st.text("Bağlantınız Güvenli:Bilgileriniz (örneğin şifreler veya kredi kartı numaraları) bu siteye gönderilirken gizli olur.")
+elif url.startswith("http"):
+    st.text("Bağlantınız Güvenli Değil:Bilgileriniz (örneğin şifreler veya kredi kartı numaraları gibi) bu siteye gönderilirken gizlenmez")
+else:
+    st.text ("Lütfen URL'yi kırpmadan giriniz!")
